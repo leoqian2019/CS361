69ce71046c2015312b937ee23183114347c94388
Leo Qian <a2573705387@gmail.com>
2022-03-01 08:52:31 -0500

    Revert "place project 4 files"
    This reverts commit aee2255fc4a0cd92d63c3ba3aa8bcf8255d6c1ce.

--- src/proj4BayyurtDimitrovQian/Main.java
+++ src/proj4BayyurtDimitrovQian/Main.java
@@ -6,26 +6,54 @@
  * Date: 2/28/2022
  */
 
-package proj04BayyurtDimitrovQian;
+package proj4BayyurtDimitrovQian;
 
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.GenericStyledArea;
+import org.fxmisc.richtext.LineNumberFactory;
+import org.fxmisc.richtext.model.Paragraph;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+import org.reactfx.collection.ListModification;
+import org.reactfx.Subscription;
 
 /**
  * The Main Class for loading the fxml file and building the stage
  *
  * @author (Izge Bayyurt, Anton Dimitrov, Leo Qian)
  */
-
 public class Main extends Application{
-
     @Override
     public void start(Stage primaryStage) throws IOException {
 
@@ -34,8 +62,6 @@ public class Main extends Application{
         Parent root = loader.load();
         primaryStage.setTitle("Project 4");
         Scene scene = new Scene(root);
-
-        //scene.getStylesheets().add(getClass().getResource("java-keywords.css").toExternalForm());
 
         // Load css file
         scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
@@ -54,6 +80,8 @@ public class Main extends Application{
 
         // Show the stage
         primaryStage.show();
+
+
 
     }
 
